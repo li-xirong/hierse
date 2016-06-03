be32f676b9697a1254dda151d68f96266fd55a94
li-xirong <xirong.li@gmail.com>
2016-06-04 07:00:03 +0800

    Update get_word2vec_model.sh

--- get_word2vec_model.sh
+++ get_word2vec_model.sh
@@ -13,6 +13,7 @@ cd $DIR
 echo "Downloading..."
 
 wget http://www.mmc.ruc.edu.cn/research/hierse/flickr4m.word2vec.tar.gz
+# or manually download the zipped file from https://drive.google.com/open?id=0B89Vll9z5OVEfnRHUWRSY0dkRjNuRVZYUGtzY0ltVTZ2bkRvSVBTRjd0akEwckVMZGV6WTQ&authuser=0
 
 echo "Unzipping..."
 
